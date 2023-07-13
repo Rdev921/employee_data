040c47f522ead1ef52481214c2f54048e445f84c
rishabh dev <rishabhd921@gmail.com>
2023-07-13 15:21:18 +0530

    employee data

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ require('dotenv').config();
 const app = express();
 
 app.use(express.json());
-app.use(auth);
 app.use('/employee',EmpRouter);
+app.use(auth);
 app.use('/employees',AddEmpRouter);
 app.listen(process.env.port,async()=>{
     try {
